dca75736d804420af04d983639830393e0a6c5c1
poa <poaetcethe2nd@gmail.com>
2024-05-20 02:22:03 +0900

    added basic argument template

--- src/lyrixir/lyrixir.py
+++ src/lyrixir/lyrixir.py
@@ -3,3 +3,11 @@ import sys
 
 def main() -> None:
     arguments: list[str] = sys.argv[1:]
+    if arguments:
+        command: str = arguments[0]
+        match command:
+            case 'add': pass
+            case 'remove': pass
+            case _: pass
+    else:
+        pass
