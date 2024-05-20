65e683f953bd0bcb271bd755c092f11aa72c68ca
poa <poaetcethe2nd@gmail.com>
2024-05-20 13:41:10 +0900

    created basic enums

---
+++ src/lyrixir/fancy.py
@@ -0,0 +1,33 @@
+import enum
+
+
+class Alignment(enum.Enum):
+    LEFT = 1
+    CENTER = 2
+    RIGHT = 3
+
+
+class Color(enum.Enum):
+    BLACK = 30
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+    MAGENTA = 35
+    CYAN = 36
+    LIGHT_GRAY = 37
+    DEFAULT = 39
+    DARK_GRAY = 90
+    LIGHT_RED = 91
+    LIGHT_GREEN = 92
+    LIGHT_YELLOW = 93
+    LIGHT_BLUE = 94
+    LIGHT_MAGENTA = 95
+    LIGHT_CYAN = 96
+
+
+class Style(enum.Enum):
+    REGULAR = 0
+    BOLD = 1
+    ITALIC = 3
+    UNDERLINE = 4
