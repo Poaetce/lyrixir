9d5d7af9fa03eead979e23848efe46b93157d459
poa <poaetcethe2nd@gmail.com>
2024-05-21 00:41:06 +0900

    created template for Song class

---
+++ src/lyrixir/song.py
@@ -0,0 +1,5 @@
+class Song:
+    def __init__(self, artist: str, album: str, lyrics: str) -> None:
+        self.artist: str = artist
+        self.album: str = album
+        self.lyrics: str = lyrics
