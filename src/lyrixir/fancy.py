e645edcfab40e610e1f126f11604784b890906de
poa <poaetcethe2nd@gmail.com>
2024-05-20 14:39:28 +0900

    added type annotation to enums

--- src/lyrixir/fancy.py
+++ src/lyrixir/fancy.py
@@ -4,32 +4,32 @@ RESET: str = '\x1b[0m'
 
 
 class Alignment(enum.Enum):
-    LEFT = 1
-    CENTER = 2
-    RIGHT = 3
+    LEFT: int = 1
+    CENTER: int = 2
+    RIGHT: int = 3
 
 
 class Color(enum.Enum):
-    BLACK = 30
-    RED = 31
-    GREEN = 32
-    YELLOW = 33
-    BLUE = 34
-    MAGENTA = 35
-    CYAN = 36
-    LIGHT_GRAY = 37
-    DEFAULT = 39
-    DARK_GRAY = 90
-    LIGHT_RED = 91
-    LIGHT_GREEN = 92
-    LIGHT_YELLOW = 93
-    LIGHT_BLUE = 94
-    LIGHT_MAGENTA = 95
-    LIGHT_CYAN = 96
+    BLACK: int = 30
+    RED: int = 31
+    GREEN: int = 32
+    YELLOW: int = 33
+    BLUE: int = 34
+    MAGENTA: int = 35
+    CYAN: int = 36
+    LIGHT_GRAY: int = 37
+    DEFAULT: int = 39
+    DARK_GRAY: int = 90
+    LIGHT_RED: int = 91
+    LIGHT_GREEN: int = 92
+    LIGHT_YELLOW: int = 93
+    LIGHT_BLUE: int = 94
+    LIGHT_MAGENTA: int = 95
+    LIGHT_CYAN: int = 96
 
 
 class Style(enum.Enum):
-    BOLD = 1
-    ITALIC = 3
-    UNDERLINE = 4
-    STRIKETHROUGH = 9
+    BOLD: int = 1
+    ITALIC: int = 3
+    UNDERLINE: int = 4
+    STRIKETHROUGH: int = 9
