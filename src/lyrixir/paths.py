b4965dd7bb7c25da9a463dc9bacedcc15454e59e
poa <poaetcethe2nd@gmail.com>
2024-05-23 23:19:06 +0900

    added paths for windows

--- src/lyrixir/paths.py
+++ src/lyrixir/paths.py
@@ -1,2 +1,14 @@
-DATA: str = '~/.local/share/lyrixir'
-CONFIG: str = '~/.config/lyrixir'
+import os
+import platform
+
+data: str
+config: str
+
+match platform.system():
+    case 'Linux':
+        data = '~/.local/share/lyrixir'
+        config = '~/.config/lyrixir'
+        
+    case 'Windows':
+        data = f'{os.getenv('APPDATA')}\\lyrixir-data'
+        config = f'{os.getenv('APPDATA')}\\lyrixir'
