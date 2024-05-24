2c0412ba2d18fb581f5c6e3b3dfc100ccb4ad4b8
poa <poaetcethe2nd@gmail.com>
2024-05-24 15:27:30 +0900

    restructured get_song

--- src/lyrixir/song.py
+++ src/lyrixir/song.py
@@ -22,13 +22,14 @@ def get_song(reference: str) -> Song | None:
 
     response: requests.Response = requests.get(url)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        page: bs4.BeautifulSoup = bs4.BeautifulSoup(response.content, 'html.parser')
+
+        lyrics: str = page.find('div', attrs = {'class': None, 'id': None}).get_text().strip()
+        artist: str = page.find('b').get_text().rsplit(' ', 1)[0]
+        title: str = page.find_all('b')[1].get_text().strip('"')
+
+        return Song(reference, artist, title, lyrics)
+
+    else:
         return None
-
-    page: bs4.BeautifulSoup = bs4.BeautifulSoup(response.content, 'html.parser')
-
-    lyrics: str = page.find('div', attrs = {'class': None, 'id': None}).get_text().strip()
-    artist: str = page.find('b').get_text().rsplit(' ', 1)[0]
-    title: str = page.find_all('b')[1].get_text().strip('"')
-
-    return Song(reference, artist, title, lyrics)
