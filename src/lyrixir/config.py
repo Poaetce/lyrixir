46bb5e1ebcee9f9b07b5887649eea9011c3fdc7d
poa <poaetcethe2nd@gmail.com>
2024-05-25 22:02:07 +0900

    minor spacing fix

--- src/lyrixir/config.py
+++ src/lyrixir/config.py
@@ -28,12 +28,14 @@ if os.path.exists(config_file_name):
 
 list_file_name = os.path.join(paths.config, 'reference.list')
 
+
 def read_reference_list() -> list[str]:
     if os.path.exists(list_file_name):
         with open(list_file_name, 'r') as file:
             return file.read().splitlines()
     else:
         return []
+
 
 def add_reference_list(reference: str) -> None:
     os.makedirs(os.path.dirname(list_file_name), exist_ok = True)
