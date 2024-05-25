70ff3b8f9d1cc5e7fef0c51a265fade861117a0f
poa <poaetcethe2nd@gmail.com>
2024-05-25 22:01:49 +0900

    moved main command flow to __main__.py and cleared up lyrixir.py

--- src/lyrixir/__main__.py
+++ src/lyrixir/__main__.py
@@ -1,5 +1,20 @@
+import sys
+
 from . import lyrixir
+from . import add
+
+
+def main() -> None:
+    arguments: list[str] = sys.argv[1:]
+    if arguments:
+        command: str = arguments[0]
+        match command:
+            case 'add': add.main(arguments[1:])
+            case 'remove': pass
+            case _: pass
+    else:
+        lyrixir.main()
 
 
 if __name__ == '__main__':
-    lyrixir.main()
+    main()
--- src/lyrixir/lyrixir.py
+++ src/lyrixir/lyrixir.py
@@ -1,15 +1,2 @@
-import sys
-
-from . import add
-
-
 def main() -> None:
-    arguments: list[str] = sys.argv[1:]
-    if arguments:
-        command: str = arguments[0]
-        match command:
-            case 'add': add.main(arguments[1:])
-            case 'remove': pass
-            case _: pass
-    else:
-        pass
+    pass
