ecfbf97f908169b5056f55a6a15db111af34b9a6
poa <poaetcethe2nd@gmail.com>
2024-06-01 23:12:26 +0900

    finished adding comments

--- src/lyrixir/lyrixir_add.py
+++ src/lyrixir/lyrixir_add.py
@@ -4,12 +4,18 @@ from . import songs
 
 
 def add_song(reference: str) -> None:
+    # tries to get the song from the reference
     try:
         song: songs.Song | None = songs.get_song(reference)
+
+    # gives error if failed
     except:
         fancy.print_error(f"unable to connect")
+
     else:
+        # if song is gotten
         if song:
+            # saves the song data and adds reference to the reference list
             song.save()
             config.add_to_reference_list(reference)
 
@@ -19,9 +25,12 @@ def add_song(reference: str) -> None:
 
 
 def add_songs(references: list[str]) -> None:
+    # gets the reference list
     reference_list: list[str] = config.read_reference_list()
 
+    # loops for each entered references
     for song_reference in references:
+        # adds song if it isn't on the reference list
         if song_reference in reference_list:
             fancy.print_neutral(f"{song_reference} is already added")
         else:
@@ -29,6 +38,7 @@ def add_songs(references: list[str]) -> None:
 
 
 def main(arguments: list[str]) -> None:
+    # makes sure references were given as arguments
     if arguments:
         add_songs(arguments)
     else:
--- src/lyrixir/lyrixir_list.py
+++ src/lyrixir/lyrixir_list.py
@@ -4,12 +4,14 @@ from . import songs
 
 
 def print_song_information(reference: str) -> None:
+    # gets the song from the reference and prints its information
     song: songs.Song | None = songs.open_song(reference)
     if song:
         fancy.print_neutral(f"{song.title} - {song.artist}")
 
 
 def list_songs(references: list[str]) -> None:
+    # prints the information of each reference
     for reference in references:
         print_song_information(reference)
 
@@ -17,14 +19,22 @@ def list_songs(references: list[str]) -> None:
 def main(arguments: list[str]) -> None:
     references: list[str]
 
+    # if arguments were given
     if arguments:
         references = []
+
+        # loops for reference in the reference list
         for reference in config.read_reference_list():
+            # gets the artist from the reference
             artist: str = reference.split('/')[0]
+
+            # adds the reference to references
             if artist in arguments:
                 references.append(reference)
 
+    # adds all references if no arguments were given
     else:
         references = config.read_reference_list()
 
+    # lists the references
     list_songs(references)
